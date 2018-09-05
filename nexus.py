2ec22705216abb3843f8727d1e777856b441e77b
MM-coder <mauromartins@email.com>
2018-09-05 18:21:52 +0100

    Started the project

---
+++ nexus.py
@@ -0,0 +1,108 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+import random
+import time
+from PIL import Image, ImageFilter
+import requests
+from io import BytesIO
+
+bot = commands.Bot(command_prefix='n!')
+bot.remove_command('help')
+async def loop():
+    while True:
+        await bot.change_presence(game=discord.Game(name="n!help", url="https://twitch.tv/MMgamerBOT", type=1))
+        await asyncio.sleep(15)
+        await bot.change_presence(game=discord.Game(name="mmgamerbot.com", url="https://twitch.tv/MMgamerBOT", type=1))
+        await asyncio.sleep(15)
+        await bot.change_presence(game=discord.Game(name="prefix -> n!", url="https://twitch.tv/MMgamerBOT", type=1))
+        await asyncio.sleep(15)
+
+@bot.event
+async def on_ready():
+    print ("Bot has Booted!")
+    print ("I am running on " + bot.user.name)
+    print ("With the ID: " + bot.user.id)
+    await bot.change_presence(game=discord.Game(name="mmgamerbot.com", url="https://twitch.tv/MMgamerBOT", type=1))
+    await loop()
+
+
+#welcome to nexus
+
+#general commands
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+        t1 = time.perf_counter()
+        tmp = await bot.say("pinging...")
+        t2 = time.perf_counter()
+        await bot.say("Ping: {}ms".format(round((t2-t1)*1000)))
+        await bot.delete_message(tmp)
+
+@bot.command(pass_context=True)
+async def help(ctx):
+    await bot.say("Bot under development")
+
+@bor.command(pass_context=True)
+async def changelog(ctx):
+    with open("changelog.txt", "r") as txtfile:
+    content = txtfile.read()
+    await bot.say("```{0}```".format(content))
+    txtfile.close()
+
+
+@bot.command(pass_context=True)
+async def lostshibe(ctx, user: discord.Member=None):
+    if user is None:
+        user = ctx.message.author
+    base = Image.open(BytesIO(requests.get('https://i.imgur.com/MRFa2n5.jpg%27').content)).convert('RGBA')
+    img = Image.open(BytesIO(requests.get(user.avatar_url).content)).convert("RGBA").resize((200, 200))
+    img = img.rotate(2, expand=1)
+    base.paste(img, (135, 170), img)
+    f = "Lostshibes/{}.png".format(user.id)
+    base.save(f)
+    await bot.send(file=discord.File(f))
+
+
+
+
+@bot.command(pass_context=True)
+async def jail(ctx, user: discord.Member):
+    if user is None:
+        pass
+    else:
+        response = requests.get(user.avatar_url)
+        background = Image.open(BytesIO(response.content)).convert("RGBA")
+        foreground = Image.open("jail.png").convert("RGBA")
+        background.paste(foreground, (0, 0), foreground)
+        background.save("jailed.png")
+        await bot.send_file(ctx.message.channel, "jailed.png")
+
+
+@bot.command(pass_context=True)
+async def gay(ctx, user: discord.Member):
+    if user is None:
+        pass
+    else:
+        response = requests.get(user.avatar_url)
+        background = Image.open(BytesIO(response.content)).convert("RGBA")
+        foreground = Image.open("gay.png").convert("RGBA")
+        foreground.putalpha(128)
+        background.paste(foreground, (0, 0), foreground)
+        background.save("gaypfp.png")
+        await bot.send_file(ctx.message.channel, "gaypfp.png")
+    
+                         
+
+
+
+
+
+
+
+
+
+
+bot.run(os.getenv('TOKEN'))
