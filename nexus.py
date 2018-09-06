5ce4ae68c1dec0f02c03c43c0e72b78e4b313b19
MM-coder <mauromartins@email.com>
2018-09-06 16:45:26 +0100

    Update nexus.py

--- nexus.py
+++ nexus.py
@@ -165,6 +165,7 @@ async def cat(ctx):
 async def doge(ctx):
         response = requests.get('http://shibe.online/api/shibes?count=1')
         data = response.json()
+        print data
         await bot.say(data['file'])
 
 
