abec06c4ee29fd6301d07abf6e42ef2fe5d5f8aa
MM-coder <mauromartins@email.com>
2018-09-09 19:53:04 +0100

    Update nexero.py

--- nexero.py
+++ nexero.py
@@ -222,7 +222,7 @@ async def birb(ctx):
 async def catfact(ctx):
         response = requests.get('https://some-random-api.ml/catfact')
         data = response.json()
-        embed = discord.Embed(title = "A random Cat Fact" description= f"{data['fact']}", color=0x23272A)
+        embed = discord.Embed(title = "A random Cat Fact" description=f"{data['fact']}", color=0x23272A)
         embed.set_thumbnail(url="https://clipart.info/images/ccovers/1522855947cute-cat-png-cartoon-clip-art.png")
         await bot.say(embed=embed)
 
