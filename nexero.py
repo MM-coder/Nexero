c364b47aabed0a3e47a3bf1e59a5becd198aed7f
MM-coder <mauromartins@email.com>
2018-09-11 16:50:57 +0100

    Added in n!invite

--- nexero.py
+++ nexero.py
@@ -53,6 +53,12 @@ async def ping(ctx):
         await bot.delete_message(tmp)
 
 @bot.command(pass_context=True)
+async def invite(ctx):
+    embed=discord.Embed(title="Invite The Bot To Your Server!",description="The bot's invite link: http://invite.nexerobot.cf", color=0x23272A)
+    embed.set_author(icon_url="https://png.icons8.com/small/1600/external-link.png",name="Link")
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
 async def help(ctx):
     with open("help.txt", "r") as txtfile:
         content = txtfile.read()
