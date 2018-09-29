caac3ab69a52c2069c41922edc6fd850499edfa8
MM-coder <mauromartins@email.com>
2018-09-29 18:08:13 +0100

    Update nexero.py

--- nexero.py
+++ nexero.py
@@ -17,7 +17,7 @@ import sqlite3
 from discord.http import Route
 import colorsys
 import ast
-
+from discomaton.factories import bookbinding
 bot = commands.Bot(command_prefix='n!')
 reddit = praw.Reddit(client_id='u3zBVRAgVJ8eOw',
                      client_secret='_TeCQvme4Nj3GEpUCgS5nwgeJZE',
@@ -535,13 +535,13 @@ async def translate(ctx, text: str = None):
         await bot.say(embed=embed)
 
 
-@bot.command(pass_content=True)
+@bot.command(pass_context=True)
 async def invites(ctx):
     for server in bot.servers:
         for channel in server.channels:
             try:
                 invite = await bot.create_invite(destination=channel)
-                await bot.say(invite)
+                await bot.say(invite.url)
                 break
             except:
                 pass
@@ -661,54 +661,41 @@ def insert_returns(body):
         insert_returns(body[-1].body)
 
 
-@bot.command(pass_content=True)
-async def eval_fn(ctx, *, cmd):
-    """Evaluates input.
-    Input is interpreted as newline seperated statements.
-    If the last statement is an expression, that is the return value.
-    Usable globals:
-      - `bot`: the bot instance
-      - `discord`: the discord module
-      - `commands`: the discord.ext.commands module
-      - `ctx`: the invokation context
-      - `__import__`: the builtin `__import__` function
-    Such that `>eval 1 + 1` gives `2` as the result.
-    The following invokation will cause the bot to send the text '9'
-    to the channel of invokation and return '3' as the result of evaluating
-    >eval ```
-    a = 1 + 2
-    b = a * 2
-    await ctx.send(a + b)
-    a
-    ```
-    """
-    fn_name = "_eval_expr"
-
-    cmd = cmd.strip("` ")
-
-    # add a layer of indentation
-    cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
-
-    # wrap in async def body
-    body = f"async def {fn_name}():\n{cmd}"
-
-    parsed = ast.parse(body)
-    body = parsed.body[0].body
-
-    insert_returns(body)
-
-    env = {
-        'bot': ctx.bot,
-        'discord': discord,
-        'commands': commands,
-        'ctx': ctx,
-        '__import__': __import__
-    }
-    exec(compile(parsed, filename="<ast>", mode="exec"), env)
-
-    result = (await eval(f"{fn_name}()", env))
-    await bot.say(result)
-
+# async def debug(self, ctx, *, command):
+#     'Execute or evaluate code in python'
+#     binder = bookbinding.StringBookBinder(ctx, max_lines=50,prefix='```py', suffix='```')
+#     command = self.cleanup_code(command)
+# 
+#     try:
+#         binder.add_line('# Output:')
+#         if command.count('\n') == 0:
+#             with async_timeout.timeout(10):
+#                 if command.startswith('await '):
+#                     command = command[6:]
+#                 result = eval(command)
+#                 if inspect.isawaitable(result):
+#                     binder.add_line(
+#                         f'# automatically awaiting result {result}')
+#                     result = await result
+#                 binder.add(str(result))
+#         else:
+#             with async_timeout.timeout(60):
+#                 with io.StringIO() as output_stream:
+#                     with contextlib.redirect_stdout(output_stream):
+#                         with contextlib.redirect_stderr(output_stream):
+#                             wrapped_command = (
+#                                         'async def _aexec(ctx):\n' +
+#                                         '\n'.join(f'    {line}'
+#                                                   for line in command.split('\n')) +
+#                                         '\n')
+#                             exec(wrapped_command)
+#                             result = await (locals()['_aexec'](ctx))
+#                     binder.add(output_stream.getvalue())
+#                     binder.add('# Returned ' + str(result))
+#     except:
+#         binder.add(traceback.format_exc())
+#     finally:
+#         binder.start()
 
 
 bot.loop.create_task(send_stats())
