ec990783b45188a47179e0f8938760cde52e9eda
MM-coder <mauromartins@email.com>
2018-09-29 17:18:11 +0100

    Update nexero.py

--- nexero.py
+++ nexero.py
@@ -76,12 +76,6 @@ async def invite(ctx):
     await bot.say(embed=embed)
 
 @bot.command(pass_context=True)
-async def invite(ctx):
-    embed=discord.Embed(title="Invite The Bot To Your Server!",description="The bot's invite link: http://invite.nexerobot.cf", color=0x08202D)
-    embed.set_author(icon_url="https://png.icons8.com/small/1600/external-link.png",name="Link")
-    await bot.say(embed=embed)
-
-@bot.command(pass_context=True)
 async def help(ctx):
     with open("help.txt", "r") as txtfile:
         content = txtfile.read()
