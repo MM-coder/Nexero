05a7fc64abd196fd21f6e2add8284c4621f481f3
MM-coder <mauromartins@email.com>
2018-10-07 14:36:42 +0100

    LOL

--- nexero.py
+++ nexero.py
@@ -160,10 +160,8 @@ async def kick(ctx, user: discord.User, *, reason: str):
 @bot.command(pass_context=True)
 async def ban(ctx, user: discord.Member, *, reason: str):
     if "n.staff" in [role.name for role in ctx.message.author.roles]:
-        casenumb = get_bans()
-        add_ban(user.id)
         await bot.ban(user)
-        embed = discord.embed(title = "Ban Issued! Case: {casenumb}", description = "Details about the ban:", color =0x08202D)
+        embed = discord.embed(title = "Ban Issued!", description = "Details about the ban:", color =0x08202D)
         embed.add_field(name = "Moderator:", value = f"{ctx.message.author.display_name}")
         embed.add_field(name = "User Banned:", value = f"{user.name}")
         embed.add_field(name = "Reason:", value = f"{reason}")
